4a10e0118596abee154c18b454cac79bd745a98b
Jenise Ruehle <jela07@gmail.com>
2020-01-08 16:19:33 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,3 +6,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def valid_move?(board, index)
+  index.between?(0, 8) && !(position_taken?(board, index))
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil 
+    return false 
+  else
+    return true 
+  end
+end
